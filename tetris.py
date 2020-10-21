de4417430e60ea18ac7becd90f46880e5cfea174
Zacky2456 <zaacky2456@gmail.com>
2020-10-21 19:23:37 +0530

    👌 The GamePlay now Functions

--- tetris.py
+++ tetris.py
@@ -1,45 +1,74 @@
 import pygame
 from tilemaps import *
-from random import choice
-from itertools import cycle
+from definations import *
 
 win = pygame.display.set_mode((1280, 720))
 running = True
+clock = pygame.time.Clock()
+FPS = 10
+TILE_SIZE : int = 35
+falling = False
+dx = 0
+active_tetrimino = [Tetrimino(), Tetrimino(), Tetrimino(), Tetrimino()]
+board = Board()
+fall_timer = FPS
 
-class Tetrimino:
-    def __init__(self, tilemap=tilemap):
-        self.shapes = tetrimino_shapes[choice(list(tetrimino_shapes))]
-        self.shape = choice(self.shapes)
-        self.shapes = cycle(self.shapes)
-        self.y = 0
-        self.x = 4
-        self.tilemap = tilemap
-    
-    def check_for_collision(self) -> bool:
-        '''Checks whether the tetrimino is in overlapping collision with the map'''
-        shape = self.shape
-        y = 0
-        for row in shape:
-            x = 0
-            for cell in row:
-                if self.tilemap[y+self.y][x+self.x] != 0: return True
-                x += 1
-            y += 1
-        
-        return False
-    
-    def __str__(self):
-        s = ""
-        for row in self.shape:
-            for cell in row:
-                if cell == 0: s = s + " "
-                else: s = s + "O"
-            s = s + '\n'
-        return str(s)
+############# Temp test surface i use in place of tile sprite till i get actual tile sprites
+tile_sprite = pygame.Surface((TILE_SIZE, TILE_SIZE))
+pygame.draw.rect(tile_sprite, (255, 255, 255), pygame.Rect(0, 0, TILE_SIZE, TILE_SIZE))
+###########################################################################################
 
 while running:
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: running = False
-    
-    pygame.display.update()
+	clock.tick(FPS) # FPS
+	win.fill((0, 0, 0)) # Cleaning the window
+	
+	# Input
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT: running = False
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				dx = -1
+			elif event.key == pygame.K_RIGHT:
+				dx = 1
+			elif event.key == pygame.K_UP:
+				active_tetrimino[0].rotate()
+			elif event.key == pygame.K_DOWN:
+				falling = True
+			elif event.key == pygame.K_SPACE:
+				active_tetrimino[0].drop()
+				active_tetrimino[0].stop()
+				del active_tetrimino[0]
+				active_tetrimino.append(Tetrimino())
+		elif event.type == pygame.KEYUP:
+			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+				dx = 0
+			elif event.key == pygame.K_DOWN:
+				falling = False
+	
+	# Making the tile Move 
+	fall_timer -= 1
+	if fall_timer <= 0 or falling:
+		prev_y = active_tetrimino[0].y
+		active_tetrimino[0].fall()
+		if prev_y == active_tetrimino[0].y:
+			active_tetrimino[0].stop()
+			del active_tetrimino[0]
+			active_tetrimino.append(Tetrimino())
+		fall_timer = FPS
+		
+	active_tetrimino[0].move_horizontally(dx)
+	
+	# Drawing the tilemap
+	y = 0
+	for row in tilemap:
+		x = 0
+		for tile in row:
+			if tile != 0: win.blit(tile_sprite, (x*TILE_SIZE, y*TILE_SIZE))
+			else: pygame.draw.rect(win, (20, 20, 20), pygame.Rect(x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE))
+			x += 1
+		y += 1
+	
+	# Updating the screen
+	board.clear_full_rows()
+	active_tetrimino[0].draw(TILE_SIZE, {1:tile_sprite}, win)
+	pygame.display.update()
