1dd2f84c4528e53f21574f0668c224836fb7ec53
Zacky2456 <zaacky2456@gmail.com>
2020-10-27 13:49:36 +0530

    😫Fixed the indentation, again

--- definations.py
+++ definations.py
@@ -3,126 +3,126 @@ from tilemaps import *
 from random import choice
 
 class Board:
-	def __init__(self, tilemap:list=tilemap):
-		self.tilemap:list = tilemap
-		
-	def clear_full_rows(self):
-		y = 0
-		empty_row = []
-		for i in range(10):
-			empty_row.append(0)
-		for row in tilemap:
-			if not 0 in row:
-				del tilemap[y]
-				tilemap.insert(0, empty_row)
-			y += 1
+    def __init__(self, tilemap:list=tilemap):
+        self.tilemap:list = tilemap
+        
+    def clear_full_rows(self):
+        y = 0
+        empty_row = []
+        for i in range(10):
+            empty_row.append(0)
+        for row in tilemap:
+            if not 0 in row:
+                del tilemap[y]
+                tilemap.insert(0, empty_row)
+            y += 1
 
-	def __str__(self):
-		s = ""
-		for row in self.tilemap:
-			for cell in row:
-				if cell == 0: s = s + " "
-				else: s = s + "XX"
-			s = s + '\n'
-		return str(s)				
+    def __str__(self):
+        s = ""
+        for row in self.tilemap:
+            for cell in row:
+                if cell == 0: s = s + " "
+                else: s = s + "XX"
+            s = s + '\n'
+        return str(s)                
 
 class Tetrimino:
-	def __init__(self, tilemap=tilemap):
-		self.shapes : list = tetrimino_shapes[choice(list(tetrimino_shapes))]
-		self.shape : list = choice(self.shapes)
-		self.y = 0
-		self.x = 4
-		self.tilemap : list = tilemap
-		if self.colliding_with_map():
-			pygame.quit()
-	
-	def draw(self, tile_size:int, tile_sprites_dict:dict, display_to_draw_to):
-		y = 0
-		for row in self.shape:
-			x = 0
-			for tile in row:
-				if tile != 0: display_to_draw_to.blit(tile_sprites_dict[tile], ((x+self.x)*tile_size, (y+self.y)*tile_size))
-				x += 1
-			y += 1
-			
-	
-	def colliding_with_map(self, shape=None) -> bool:
-		'''Checks whether the tetrimino is in overlapping collision with the map, Also checks if the tetrimino is out of the map or not'''
-		if shape == None:
-			shape = self.shape
-		y = 0
-		for row in shape:
-			x = 0
-			for tile in row:
-				if tile != 0:
-					try:
-						if tilemap[y+self.y][x+self.x] != 0 or self.x+x < 0:
-							return True
-					except IndexError:
-						return True
-				x += 1
-			y += 1
-		
-		return False
-	
-	def drop(self):
-		'''Same as self.fall() but instantly makes the thing fall, rather than making it fall one tile'''
-		while not self.colliding_with_map():
-			self.y += 1
-		self.y -= 1
-	
-	def stop(self) -> None:
-		shape = self.shape
-		tilemap = self.tilemap
-		y = 0
-		for row in shape:
-			x = 0
-			for tile in row:
-				if tile != 0:
-					tilemap[y+self.y][x+self.x] = tile
-				x += 1
-			y += 1
-	
-	def will_collide_after_rotation(self) -> bool:
-		'''Checks whether the tetrimino is in overlapping collision with the map if it were to be rotated'''
-		try:
-			shape : list = self.shapes[self.shapes.index(self.shape)+1]
-		except IndexError:
-			shape : list = self.shapes[0]
-		
-		return self.colliding_with_map(shape=shape)
-	
-	def move_horizontally(self, direction:int):
-		'''Moves the tetrimino right or left, based on the direction provided to move in, 
-		direction=1 for right and -1 for left, collision is handled inside this function itself'''
-		if not self.colliding_with_map():
-			self.x += direction
-		if self.colliding_with_map():
-			self.x -= direction
-		
-	def rotate(self):
-		'''Rotates the tetrimino, i.e., changes the shape to next(shapes)'''
-		if not self.will_collide_after_rotation():
-			try:
-				self.shape : list = self.shapes[self.shapes.index(self.shape)+1]
-			except IndexError:
-				self.shape : list = self.shapes[0]
-		
-	def fall(self):
-		'''similar to `self.move_horizontally()` but moves the tetrimino vertically down instead'''
-		if not self.colliding_with_map():
-			self.y += 1
-		if self.colliding_with_map():
-			self.y -= 1
+    def __init__(self, tilemap=tilemap):
+        self.shapes : list = tetrimino_shapes[choice(list(tetrimino_shapes))]
+        self.shape : list = choice(self.shapes)
+        self.y = 0
+        self.x = 4
+        self.tilemap : list = tilemap
+        if self.colliding_with_map():
+            pygame.quit()
+    
+    def draw(self, tile_size:int, tile_sprites_dict:dict, display_to_draw_to):
+        y = 0
+        for row in self.shape:
+            x = 0
+            for tile in row:
+                if tile != 0: display_to_draw_to.blit(tile_sprites_dict[tile], ((x+self.x)*tile_size, (y+self.y)*tile_size))
+                x += 1
+            y += 1
+            
+    
+    def colliding_with_map(self, shape=None) -> bool:
+        '''Checks whether the tetrimino is in overlapping collision with the map, Also checks if the tetrimino is out of the map or not'''
+        if shape == None:
+            shape = self.shape
+        y = 0
+        for row in shape:
+            x = 0
+            for tile in row:
+                if tile != 0:
+                    try:
+                        if tilemap[y+self.y][x+self.x] != 0 or self.x+x < 0:
+                            return True
+                    except IndexError:
+                        return True
+                x += 1
+            y += 1
+        
+        return False
+    
+    def drop(self):
+        '''Same as self.fall() but instantly makes the thing fall, rather than making it fall one tile'''
+        while not self.colliding_with_map():
+            self.y += 1
+        self.y -= 1
+    
+    def stop(self) -> None:
+        shape = self.shape
+        tilemap = self.tilemap
+        y = 0
+        for row in shape:
+            x = 0
+            for tile in row:
+                if tile != 0:
+                    tilemap[y+self.y][x+self.x] = tile
+                x += 1
+            y += 1
+    
+    def will_collide_after_rotation(self) -> bool:
+        '''Checks whether the tetrimino is in overlapping collision with the map if it were to be rotated'''
+        try:
+            shape : list = self.shapes[self.shapes.index(self.shape)+1]
+        except IndexError:
+            shape : list = self.shapes[0]
+        
+        return self.colliding_with_map(shape=shape)
+    
+    def move_horizontally(self, direction:int):
+        '''Moves the tetrimino right or left, based on the direction provided to move in, 
+        direction=1 for right and -1 for left, collision is handled inside this function itself'''
+        if not self.colliding_with_map():
+            self.x += direction
+        if self.colliding_with_map():
+            self.x -= direction
+        
+    def rotate(self):
+        '''Rotates the tetrimino, i.e., changes the shape to next(shapes)'''
+        if not self.will_collide_after_rotation():
+            try:
+                self.shape : list = self.shapes[self.shapes.index(self.shape)+1]
+            except IndexError:
+                self.shape : list = self.shapes[0]
+        
+    def fall(self):
+        '''similar to `self.move_horizontally()` but moves the tetrimino vertically down instead'''
+        if not self.colliding_with_map():
+            self.y += 1
+        if self.colliding_with_map():
+            self.y -= 1
 
-	def __str__(self):
-		s = ""
-		for row in self.shape:
-			for cell in row:
-				if cell == 0: s = s + " "
-				else: s = s + "XX"
-			s = s + '\n'
-		return str(s)
-	
+    def __str__(self):
+        s = ""
+        for row in self.shape:
+            for cell in row:
+                if cell == 0: s = s + " "
+                else: s = s + "XX"
+            s = s + '\n'
+        return str(s)
+    
 if __name__ == '__main__':
-	__import__('tetris')
+    __import__('tetris')
--- tetris.py
+++ tetris.py
@@ -6,69 +6,74 @@ win = pygame.display.set_mode((1280, 720))
 running = True
 clock = pygame.time.Clock()
 FPS = 10
-TILE_SIZE : int = 35
+TILE_SIZE: int = 35
 falling = False
 dx = 0
 active_tetrimino = [Tetrimino(), Tetrimino(), Tetrimino(), Tetrimino()]
 board = Board()
 fall_timer = FPS
 
-############# Temp test surface i use in place of tile sprite till i get actual tile sprites
+# Temp test surface i use in place of tile sprite till i get actual tile sprites
 tile_sprite = pygame.Surface((TILE_SIZE, TILE_SIZE))
-pygame.draw.rect(tile_sprite, (255, 255, 255), pygame.Rect(0, 0, TILE_SIZE, TILE_SIZE))
+pygame.draw.rect(tile_sprite, (255, 255, 255),
+                 pygame.Rect(0, 0, TILE_SIZE, TILE_SIZE))
 ###########################################################################################
 
 while running:
-	clock.tick(FPS) # FPS
-	win.fill((0, 0, 0)) # Cleaning the window
-	
-	# Input
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT: running = False
-		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LEFT:
-				dx = -1
-			elif event.key == pygame.K_RIGHT:
-				dx = 1
-			elif event.key == pygame.K_UP:
-				active_tetrimino[0].rotate()
-			elif event.key == pygame.K_DOWN:
-				falling = True
-			elif event.key == pygame.K_SPACE:
-				active_tetrimino[0].drop()
-				active_tetrimino[0].stop()
-				del active_tetrimino[0]
-				active_tetrimino.append(Tetrimino())
-		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-				dx = 0
-			elif event.key == pygame.K_DOWN:
-				falling = False
-	
-	# Making the tile Move 
-	fall_timer -= 1
-	if fall_timer <= 0 or falling:
-		prev_y = active_tetrimino[0].y
-		active_tetrimino[0].fall()
-		if prev_y == active_tetrimino[0].y:
-			active_tetrimino[0].stop()
-			del active_tetrimino[0]
-			active_tetrimino.append(Tetrimino())
-		fall_timer = FPS
-		
-	active_tetrimino[0].move_horizontally(dx)
-	
-	# Drawing the tilemap
-	y = 0
-	for row in tilemap:
-		x = 0
-		for tile in row:
-			if tile != 0: win.blit(tile_sprite, (x*TILE_SIZE, y*TILE_SIZE))
-			else: pygame.draw.rect(win, (20, 20, 20), pygame.Rect(x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE))
-			x += 1
-		y += 1
-	
-	# Updating the screen
-	board.clear_full_rows()
-	active_tetrimino[0].draw(TILE_SIZE, {1:tile_sprite}, win)
-	pygame.display.update()
+    clock.tick(FPS)  # FPS
+    win.fill((0, 0, 0))  # Cleaning the window
+
+    # Input
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                dx = -1
+            elif event.key == pygame.K_RIGHT:
+                dx = 1
+            elif event.key == pygame.K_UP:
+                active_tetrimino[0].rotate()
+            elif event.key == pygame.K_DOWN:
+                falling = True
+            elif event.key == pygame.K_SPACE:
+                active_tetrimino[0].drop()
+                active_tetrimino[0].stop()
+                del active_tetrimino[0]
+                active_tetrimino.append(Tetrimino())
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                dx = 0
+            elif event.key == pygame.K_DOWN:
+                falling = False
+
+    # Making the tile Move
+    fall_timer -= 1
+    if fall_timer <= 0 or falling:
+        prev_y = active_tetrimino[0].y
+        active_tetrimino[0].fall()
+        if prev_y == active_tetrimino[0].y:
+            active_tetrimino[0].stop()
+            del active_tetrimino[0]
+            active_tetrimino.append(Tetrimino())
+        fall_timer = FPS
+
+    active_tetrimino[0].move_horizontally(dx)
+
+    # Drawing the tilemap
+    y = 0
+    for row in tilemap:
+        x = 0
+        for tile in row:
+            if tile != 0:
+                win.blit(tile_sprite, (x*TILE_SIZE, y*TILE_SIZE))
+            else:
+                pygame.draw.rect(win, (20, 20, 20), pygame.Rect(
+                    x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE))
+            x += 1
+        y += 1
+
+    # Updating the screen
+    board.clear_full_rows()
+    active_tetrimino[0].draw(TILE_SIZE, {1: tile_sprite}, win)
+    pygame.display.update()
